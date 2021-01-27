003c65e568ed4a1c66465b344648a0424cdc46e0
Stephen Axtell <axtell.stephen@gmail.com>
2021-01-27 09:38:56 +0200

    created and styled navbar component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import About from './components/About';
 import SinglePost from './components/SinglePost';
 import Post from './components/Post';
 import Project from './components/Project';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route component={Home} path="/" exact />
         <Route component={About} path="/about" />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Navbar() {
   return (
-    <h2>Navbar</h2>
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink 
+            to="/home" 
+            title="Home" 
+            exact
+            activeClassName="text-white" 
+            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+            Stephen
+          </NavLink>
+          <NavLink 
+            to="/post" 
+            title="Blog Posts"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">Blog Posts</NavLink>
+          <NavLink 
+            to="/project" 
+            title="Projects"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+            Projects
+          </NavLink>
+          <NavLink 
+            to="/about" 
+            title="About Me"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+            About Me!
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon url="https://github.com/axtell5000" title="Github account" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
+          <SocialIcon url="https://www.linkedin.com/in/stephen-axtell-1ab27754/" title="Linkedin account" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
+        </div>
+      </div>
+    </header>
   );
 }
