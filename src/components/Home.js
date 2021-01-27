6ef0ba40bf62b0bd809b322dd891aebceb01b6c2
Stephen Axtell <axtell.stephen@gmail.com>
2021-01-27 10:13:29 +0200

    added home page and stlying

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import image from '../water-unsplash.jpg';
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <main>
+      <img src={image} alt="Water unsplash" className="absolute object-cover w-full h-full" />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello, I'm Stephen Axtell</h1>
+      </section>
+    </main>
   );
 }
